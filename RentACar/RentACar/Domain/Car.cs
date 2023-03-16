ecd97370465befeb1688877e942661220c44c3bc
Natasa Gavrilovic <gavrilovicnatasa123@gmail.com>
2023-03-16 09:02:45 +0100

    Adding new project
    This project is for Liskov Substitution Principle and Interface Segregation.

---
+++ RentACar/RentACar/Domain/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.Domain
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public char Offer { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
