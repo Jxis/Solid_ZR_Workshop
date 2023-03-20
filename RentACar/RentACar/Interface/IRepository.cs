47a5c2519976cf781b42ecd39c71c6e45261b577
Natasa Gavrilovic <gavrilovicnatasa123@gmail.com>
2023-03-20 16:00:21 +0100

    Added Repository for car

---
+++ RentACar/RentACar/Interface/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using RentACar.Domain;
+
+namespace RentACar.Interface
+{
+    public interface IRepository<in T> where T : Car
+    {
+        void Add(T entity);
+        List<Car> GetAll();
+        void Delete(T entity);
+        List<Car> GetByModel(string Model);
+        List<Car> GetByBusinessOffer(char offer);
+    }
+}
---
+++ RentACar/RentACar/Repository/CarRepository.cs
@@ -0,0 +1,63 @@
+﻿using RentACar.Domain;
+using RentACar.Interface;
+
+namespace RentACar.Repository
+{
+    public class CarRepository : IRepository<Car>
+    {
+        public List<Car> cars = new List<Car>();
+
+        public void Add(Car entity)
+        {
+            cars.Add(entity);
+        }
+
+        public void Delete(Car entity)
+        {
+            List<Car> cars = GetAll();
+            foreach (Car car in cars)
+            {
+                if (car == entity)
+                {
+                    cars.Remove(car);
+                }
+            }
+            
+        }
+
+        public List<Car> GetAll()
+        {
+            return cars;
+        }
+
+        public List<Car> GetByBusinessOffer(char offer)
+        {
+            List<Car> allCars = GetAll();
+            List<Car> carsOffer = new List<Car>();
+
+            foreach (var item in allCars)
+            { 
+                if(item.BusinessOffer == offer)
+                    carsOffer.Add(item);
+            }
+
+            return carsOffer;
+        }
+
+        public List<Car> GetByModel(string Model)
+        {
+            List<Car> allCars = GetAll();
+            List<Car> answer = new List<Car>();
+            foreach (var item in allCars)
+            {
+                if (item.Model == Model)
+                {
+                    answer.Add(item);
+                    item.GetPrintableString();
+                }
+            }
+
+            return answer;
+        }
+    }
+}
