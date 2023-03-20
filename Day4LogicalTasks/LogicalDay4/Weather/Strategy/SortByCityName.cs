428ad91e75c9b9a32a796d10ca018a247dd35cc2
Natasa Gavrilovic <gavrilovicnatasa123@gmail.com>
2023-03-20 12:16:57 +0100

    Changed Sorting Algorithm using Bubble Sort

--- Day4LogicalTasks/LogicalDay4/Weather/Strategy/SortByCityName.cs
+++ Day4LogicalTasks/LogicalDay4/Weather/Strategy/SortByCityName.cs
@@ -4,9 +4,34 @@
     {
         public override void Sort(List<Weather.Common.Weather> list)
         {
-            List<Weather.Common.Weather> sorted = list.OrderBy(x => x.City).ToList();
-            foreach (var item in sorted)
+            // USING LINQ
+            //List<Weather.Common.Weather> sorted = list.OrderBy(x => x.City).ToList();
+            //foreach (var item in sorted)
+            //    Console.WriteLine(item.GetPrintableString());
+
+            // USING BUBBLE SORT
+            int n = list.Count;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < n; i++)
+                {
+                    //Ako su jednaki vraca 0
+                    if (string.Compare(list[i - 1].City, list[i].City) > 0)
+                    {
+                        Weather.Common.Weather temp = list[i - 1];
+                        list[i - 1] = list[i];
+                        list[i] = temp;
+                        swapped = true;
+                    }
+                }
+                n--;
+            } while (swapped);
+
+            foreach (var item in list)
                 Console.WriteLine(item.GetPrintableString());
         }
+
     }
 }
--- Day4LogicalTasks/LogicalDay4/Weather/Strategy/SortByTemperatureAscending.cs
+++ Day4LogicalTasks/LogicalDay4/Weather/Strategy/SortByTemperatureAscending.cs
@@ -2,11 +2,36 @@
 {
     public class SortByTemperatureAscending : SortStrategy
     {
+        public List<int> Temperatures = new List<int>();
         public override void Sort(List<Weather.Common.Weather> list)
         {
-            List<Weather.Common.Weather> sorted = list.OrderBy(x => x.Temperature).ToList();
-            foreach(var item in sorted)
+            // USING LINQ
+            //List<Weather.Common.Weather> sorted = list.OrderBy(x => x.Temperature).ToList();
+            //foreach(var item in sorted)
+            //    Console.WriteLine(item.GetPrintableString());
+
+            // USING BUBBLE SORT        
+            int n = list.Count;
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < n; i++)
+                {
+                    if (list[i - 1].Temperature > list[i].Temperature)
+                    {
+                        Weather.Common.Weather temp = list[i - 1];
+                        list[i - 1] = list[i];
+                        list[i] = temp;
+                        swapped = true;
+                    }
+                }
+                n--;
+            } while (swapped);
+
+            foreach (var item in list)
                 Console.WriteLine(item.GetPrintableString());
         }
+     
     }
 }
